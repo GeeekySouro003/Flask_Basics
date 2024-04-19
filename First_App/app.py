31e02825c1ba8bf601522448be56f14f17132421
Souradip <dgsouro77@gmail.com>
2024-04-20 01:31:17 +0530

    Learned about Different routing methods and custom responses and Status Codes

--- First_App/app.py
+++ First_App/app.py
@@ -1,12 +1,18 @@
-from flask import Flask,request
+from flask import Flask,request,make_response
 
 app=Flask(__name__)
 
 
 
-@app.route('/hello') ## Differrent ways of route creation
+@app.route('/hello',methods=['POST','GET']) ## Differrent ways of route creation
 def hello():
-    return "<h2>Hello Flask!</h2>"
+    if request.method == 'GET':
+        return 'You made a successful GET request'
+    elif request.method == 'POST':
+        return 'You made a successful POST request'
+    else:
+        return "You made no request"
+    ##return "<h2>Hello Flask!</h2>"
 @app.route('/') 
 def index():
     return "<h1>Hello World</h1>"
@@ -28,5 +34,15 @@ def handle_url():
         return f"{greeting},{name}"
     else:
         return "Some parameters are missing"
+    
+    
+    
+@app.route('/hello') ## Differrent ways of route creation
+def hello():
+    response=make_response('Hello World\n')
+    response.status_code=203
+    response.headers['Content-Type']='application/json'
+    ##return "Hello Namaste!\n",200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5555,debug=True)
